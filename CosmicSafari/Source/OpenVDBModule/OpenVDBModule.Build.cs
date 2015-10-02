c5d21265536b98a56101411391bb9b42819ec828
Zachary Ladlie <zachary.ladlie@gmail.com>
2015-10-02 00:53:47 -0500

    Have openvdb building into a game module using my own built binaries of openexr

--- CosmicSafari/Source/OpenVDBModule/OpenVDBModule.Build.cs
+++ CosmicSafari/Source/OpenVDBModule/OpenVDBModule.Build.cs
@@ -2,20 +2,30 @@
 
 using UnrealBuildTool;
 using System.IO;
+using System;
 
 public class OpenVDBModule : ModuleRules
 {
 	public OpenVDBModule(TargetInfo Target)
 	{
-        Type = ModuleType.CPlusPlus;
-        //Type = ModuleType.External;
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        Platform = Target.Platform;
+        Config = Target.Configuration;
 
-        PublicIncludePaths.AddRange(new string[] { Path.Combine(ModulePath, "Public"), Path.Combine(ModulePath, "..", "..", "ThirdParty", "libovdb") });
-        PrivateIncludePaths.AddRange(new string[] { ModulePath, Path.Combine(ModulePath, "Private") });
+        //Type = ModuleType.CPlusPlus;
+        Type = ModuleType.External;
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        //System.Console.WriteLine(string.Format("ModulePath {0}:\nThirdPartyPath: {1}\nThirdPublicIncludes: {2}\nPrivateIncludes: {3}\nPublicLibraryPaths: {4}\nPublicAdditionalLibraries: {5}\nPlatformPath: {6}\nConfigurationPath: {7}", ModulePath, ThirdPartyPath, PublicIncludes, PrivateIncludes, LibPaths, LibNames, PlatformPath, ConfigurationPath));
+        PublicIncludePaths.AddRange(PublicIncludes);
+        PrivateIncludePaths.AddRange(PrivateIncludes);
         PublicLibraryPaths.AddRange(LibPaths);
         PublicAdditionalLibraries.AddRange(LibNames);
+
+        //AddThirdPartyPrivateStaticDependencies(Target, );
+        Definitions.AddRange(new string[] { "OPENVDB_OPENEXR_STATICLIB", "OPENVDB_STATICLIB" });
 	}
+
+    private UnrealTargetPlatform Platform;
+    private UnrealTargetConfiguration Config;
 
     private string ModulePath
     {
@@ -25,14 +35,74 @@ public class OpenVDBModule : ModuleRules
         }
     }
 
+    private string ThirdPartyPath
+    {
+        get
+        {
+            return Path.Combine(ModulePath, "..", "..", "ThirdParty");
+        }
+    }
+
+    private string PlatformPath
+    {
+        get
+        {
+            string path = "Win32";
+            if (Platform == UnrealTargetPlatform.Win64)
+            {
+                path = "x64";
+            }
+            return path;
+        }
+    }
+
+    private string ConfigurationPath
+    {
+        get
+        {
+            //UnrealTargetConfiguration doesn't have a field for Release, so I guess default to Release
+            string path = "Release";
+            if (Config == UnrealTargetConfiguration.Debug ||
+                Config == UnrealTargetConfiguration.DebugGame ||
+                Config == UnrealTargetConfiguration.Development)
+            {
+                path = "Debug";
+            }
+            return path;
+        }
+    }
+
+    private string[] PublicIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+                Path.Combine(ModulePath, "Public"),
+                Path.Combine(ThirdPartyPath, "libovdb")
+            };
+        }
+    }
+
+    private string[] PrivateIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+                Path.Combine(ModulePath, "Private")
+            };
+        }
+    }
+
     private string[] LibPaths
     {
         get
         {
             return new string[]
             {
-                Path.Combine(ModulePath, "..", "..", "ThirdParty", "Build", "x64", "Debug"),
-                Path.Combine(ModulePath, "..", "..", "ThirdParty", "OpenVDB", "dependencies", "lib", "x64")
+                Path.Combine(ThirdPartyPath, "Build", PlatformPath, ConfigurationPath),
+                Path.Combine(ThirdPartyPath, "OpenVDB", "dependencies", "lib", PlatformPath)
             };
         }
     }
@@ -41,7 +111,10 @@ public class OpenVDBModule : ModuleRules
     {
         get
         {
-            return new string[] { "libovdb.lib" };
+            return new string[]
+            {
+                "libovdb.lib"
+            };
         }
     }
 }
