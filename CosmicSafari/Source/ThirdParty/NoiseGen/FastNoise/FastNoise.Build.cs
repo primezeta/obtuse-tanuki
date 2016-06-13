e14fab9cf2a70bbcea27566bd4215f2d5c044549
Zachary Ladlie <zachary.ladlie@gmail.com>
2016-06-13 15:42:15 -0500

    Removed cached references to FastNoise and FastNoiseSIMD which apparently where referenced as submodules when copying the code over and the .git folder was copied

---
+++ CosmicSafari/Source/ThirdParty/NoiseGen/FastNoise/FastNoise.Build.cs
@@ -0,0 +1,112 @@
+﻿// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+using System;
+
+public class FastNoise : ModuleRules
+{
+    private UnrealTargetPlatform Platform;
+    private UnrealTargetConfiguration Configuration;
+    
+    public LibNoise(TargetInfo Target)
+    {
+        Platform = Target.Platform;
+        Configuration = Target.Configuration;
+        Type = ModuleType.External;
+        OptimizeCode = CodeOptimization.InNonDebugBuilds;
+        PublicIncludePaths.AddRange(PublicIncludes);
+        PrivateIncludePaths.AddRange(PrivateIncludes);
+        PublicSystemIncludePaths.AddRange(ThirdPartyIncludes);
+        PublicLibraryPaths.AddRange(ThirdPartyLibPaths);
+        PublicAdditionalLibraries.AddRange(ThirdPartyLibNames);
+    }
+    
+    private string PlatformPath
+    {
+        get
+        {
+            string path = "Win32";
+            if (Platform == UnrealTargetPlatform.Win64)
+            {
+                path = "x64";
+            }
+            return path;
+        }
+    }
+
+    private string ConfigurationPath
+    {
+        get
+        {
+            string path = "Release";
+            if(Configuration == UnrealTargetConfiguration.Debug ||
+               Configuration == UnrealTargetConfiguration.Development)
+            {
+                path = "Debug";
+            }
+            return path;
+        }
+    }
+
+    private string[] PublicIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+                Path.Combine(ModuleDirectory),
+            };
+        }
+    }
+
+    private string[] PrivateIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+            };
+        }
+    }
+
+    private string ThirdPartyPath
+    {
+        get
+        {
+            return Path.Combine(ModuleDirectory, "..", "..");
+        }
+    }
+
+    private string[] ThirdPartyIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+            };
+        }
+    }
+
+    private string[] ThirdPartyLibPaths
+    {
+        get
+        {
+            return new string[]
+            {
+                Path.Combine(ModuleDirectory, "Binaries", "VS2015", PlatformPath, ConfigurationPath),
+            };
+        }
+    }
+
+    private string[] ThirdPartyLibNames
+    {
+        get
+        {
+            return new string[]
+            {
+                "FastNoise.lib",
+            };
+        }
+    }    
+}
---
+++ CosmicSafari/Source/ThirdParty/NoiseGen/FastNoiseSIMD/FastNoiseSIMD.Build.cs
@@ -0,0 +1,124 @@
+﻿// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+using System;
+
+enum SimdArch { AVX, AVX2 } //TODO: Add other architectures
+
+public class LibNoise : ModuleRules
+{
+    private UnrealTargetPlatform Platform;
+    private UnrealTargetConfiguration Configuration;
+    
+    public LibNoise(TargetInfo Target)
+    {
+        Platform = Target.Platform;
+        Configuration = Target.Configuration;
+        Type = ModuleType.External;
+        OptimizeCode = CodeOptimization.InNonDebugBuilds;
+        SimdArch Arch = Arch::AVX2;
+        PublicIncludePaths.AddRange(PublicIncludes);
+        PrivateIncludePaths.AddRange(PrivateIncludes);
+        PublicSystemIncludePaths.AddRange(ThirdPartyIncludes);
+        PublicLibraryPaths.AddRange(ThirdPartyLibPaths);
+        PublicAdditionalLibraries.AddRange(ThirdPartyLibNames);
+
+        if (Arch == AVX)
+        {
+            Definitions.Add("_AVX_");
+        }
+        else if (Arch == AVX2)
+        {
+            Definitions.Add("_AVX2_");
+        }
+    }
+    
+    private string PlatformPath
+    {
+        get
+        {
+            string path = "Win32";
+            if (Platform == UnrealTargetPlatform.Win64)
+            {
+                path = "x64";
+            }
+            return path;
+        }
+    }
+
+    private string ConfigurationPath
+    {
+        get
+        {
+            string path = "Release";
+            if(Configuration == UnrealTargetConfiguration.Debug ||
+               Configuration == UnrealTargetConfiguration.Development)
+            {
+                path = "Debug";
+            }
+            return path;
+        }
+    }
+
+    private string[] PublicIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+                Path.Combine(ModuleDirectory),
+            };
+        }
+    }
+
+    private string[] PrivateIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+            };
+        }
+    }
+
+    private string ThirdPartyPath
+    {
+        get
+        {
+            return Path.Combine(ModuleDirectory, "..", "..");
+        }
+    }
+
+    private string[] ThirdPartyIncludes
+    {
+        get
+        {
+            return new string[]
+            {
+            };
+        }
+    }
+
+    private string[] ThirdPartyLibPaths
+    {
+        get
+        {
+            return new string[]
+            {
+                Path.Combine(ModuleDirectory, "Binaries", "VS2015", PlatformPath, ConfigurationPath),
+            };
+        }
+    }
+
+    private string[] ThirdPartyLibNames
+    {
+        get
+        {
+            return new string[]
+            {
+                "FastNoiseSIMD.lib",
+            };
+        }
+    }    
+}
